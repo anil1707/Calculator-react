f3bc9fe6d844ed064f50b677218e57e09ca6e52b
Anil Kumar Yadav <yadavanil1707ax@gmail.com>
2022-08-18 02:52:50 +0530

    add new file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import "./style.css"; //  for styling
 function App() {
+  const [str, setStr] = useState("");
+  function handleInput(e) {
+    setStr(str.concat(e.target.value));
+  }
+
+  //  calculate expression
+  function calculate(e) {
+    let res = eval(str);
+    setStr(res + "");
+  }
+
+  // clear all
+  function clear() {
+    setStr("");
+  }
+
+  //  functionality of backspace button
+  function backspace(e) {
+    setStr(str.slice(0, str.length - 1));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc">
+      <input type="text" placeholder="0" className="ans" value={str} />
+      <input type="button" value="9" className="button" onClick={handleInput} />
+      <input type="button" value="8" className="button" onClick={handleInput} />
+      <input type="button" value="7" className="button" onClick={handleInput} />
+      <input
+        type="button"
+        value="Del"
+        className="button btn2 btnOp"
+        onClick={backspace}
+      />
+      <input type="button" value="6" className="button" onClick={handleInput} />
+      <input type="button" value="5" className="button" onClick={handleInput} />
+      <input type="button" value="4" className="button" onClick={handleInput} />
+      <input
+        type="button"
+        value="/"
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      <input type="button" value="3" className="button" onClick={handleInput} />
+      <input type="button" value="2" className="button" onClick={handleInput} />
+      <input type="button" value="1" className="button" onClick={handleInput} />
+      <input
+        type="button"
+        value="*"
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      <input
+        type="button"
+        value="%"
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      <input type="button" value="0" className="button" onClick={handleInput} />
+      <input
+        type="button"
+        value="."
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      <input
+        type="button"
+        value="="
+        className="button btn1 btn3"
+        onClick={calculate}
+      />
+      <input type="button" value="00" className="button" onClick={handleInput} />
+      <input type="button" value="C" className="button btnOp" onClick={clear} />
+      
+      <input
+        type="button"
+        value="-"
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      
+      
+      
+      <input
+        type="button"
+        value="+"
+        className="button btnOp"
+        onClick={handleInput}
+      />
+      
+      
     </div>
   );
 }
